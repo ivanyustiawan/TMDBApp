dd41d119c65d0a330061d64dbbd8979cd15684fc
Agung Insani Alam <160093700+agunginsani99@users.noreply.github.com>
2025-05-21 22:50:36 +0700

    add viewmodel

---
+++ app/src/main/java/com/example/tmdbapp/pages/RatedViewModel.kt
@@ -0,0 +1,38 @@
+package com.example.tmdbapp.pages
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.tmdbapp.core.AppUiState
+import com.example.tmdbapp.domain.model.Movie
+import com.example.tmdbapp.domain.usecase.GetRatedMoviesUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class RatedViewModel @Inject constructor(
+    private val getRatedMoviesUseCase: GetRatedMoviesUseCase
+) : ViewModel() {
+
+    private val _uiState = MutableStateFlow<AppUiState<List<Movie>>>(AppUiState.Loading)
+    val uiState = _uiState.asStateFlow()
+
+    private fun getMovies() {
+        viewModelScope.launch {
+            getRatedMoviesUseCase(1)
+                .onStart {
+                    _uiState.value = AppUiState.Loading
+                }
+                .catch { e ->
+                    _uiState.value = AppUiState.Error(e.localizedMessage ?: "Unexpected Error")
+                }
+                .collect { movies ->
+                    _uiState.value = AppUiState.Success(movies)
+                }
+        }
+    }
+}
