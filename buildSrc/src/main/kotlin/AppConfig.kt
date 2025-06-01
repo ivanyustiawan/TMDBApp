db357765c8bd340af55c8684e17d7706748fccc8
ivanyustiawan <ivanyustiawan8@gmail.com>
2025-06-01 22:25:29 +0700

    change package name

--- buildSrc/src/main/kotlin/AppConfig.kt
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -1,5 +1,5 @@
 object AppConfig {
-    const val APPLICATION_ID = "com.movie"
+    const val APPLICATION_ID = "com.tmdbapp"
     const val COMPILE_SDK = 35
     const val TARGET_SDK = 35
     const val MIN_SDK = 29
